408361f1c964c2288f1813e165827b27cae7bdae
ivanhern3868 <83474382+ivanhern3868@users.noreply.github.com>
2022-05-14 13:32:10 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Write your JS below
+
+$(document).ready(function() {
+    console.log('Document ready')
+});
+
+/*Get Viewport Height and Width*/
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+var x = document.getElementById("demo");
+x.innerHTML = "Browser width: " + w + ", height: " + h + ".";
+/*Get Viewport Height and Width*/
